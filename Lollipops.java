fa585bb712e3df4e08d4f9ec8ee448f6b9da3edc
Cameron George <cameron.george@student.onslow.school.nz>
2020-04-15 10:32:07 +1200

    testing connection to github, Created new "Lollipop" class

---
+++ Lollipops.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Lollipops here.
+ *
+ * @author Cameron George
+ * @version 4/15/20
+ */
+public class Lollipops
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Lollipops
+     */
+    public Lollipops()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        System.out.println("hello world");
+        return x + y;
+    }
+}
